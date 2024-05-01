2bf24f192c2298cdd51967af5ab6eeedc62129e9
Nicolas-jchr <Nicolas.jchr@gmail.com>
2024-05-01 04:41:47 +0200

    Killed some dead code + modify router api

--- server/app/routers/api/router.js
+++ server/app/routers/api/router.js
@@ -2,20 +2,20 @@ const express = require("express");
 
 const router = express.Router();
 
+// For sayActions
+
 const { sayWelcome } = require("../../controllers/sayActions");
 
 router.get("/", sayWelcome);
-
-// ...
-
-/* ************************************************************************* */
-// Import And Use Routers Here
-/* ************************************************************************* */
 
 const itemsRouter = require("./items/router");
 
 router.use("/items", itemsRouter);
 
-/* ************************************************************************* */
+// For programActions
+
+const { browse } = require("../../controllers/programActions");
+
+router.get("/", browse);
 
 module.exports = router;
